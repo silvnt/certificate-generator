add2383ac59ee9967b6c1452cb57b4c3f79c5e1f
Silvano Neto <silvnto@gmail.com>
2018-07-19 17:55:56 -0300

    Environment Variables local.env to set new variables and Main checks

--- certifier.go
+++ certifier.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"errors"
+	"os"
+
 	"github.com/silvnt/certifier-go/controller"
 )
 
 func main() {
-	controller.StartServer("localhost:3000", "home.html")
+	mailerKey := os.Getenv("SENDGRID_API_KEY")
+
+	if mailerKey == "" {
+		panic(errors.New("SENDGRID_API_KEY not set. Enter the value in local.env" +
+			" and execute: source local.env"))
+	}
+
+	address := os.Getenv("ALTER_SERVER_ADDRESS")
+
+	if address != "" {
+		controller.StartServer(address, "home.html")
+	} else {
+		controller.StartServer("localhost:3000", "home.html")
+	}
+
 }
